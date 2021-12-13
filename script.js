8ddde2a57408f5d68b37b11422ef0a6b421adcb1
Raj Sri Shanker <raj.ss166@gmail.com>
2021-12-13 17:07:24 -0500

    Working Clicks 🖱️

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-window.addEventListener('click', function () {
-    var video = document.querySelector('#arvideo');
-    if (video.paused == true) {
-      video.play();
-      } else {
-      video.pause();
-    }
-  }, false);
+document.addEventListener("DOMContentLoaded", function() {
+    const sceneEl = document.querySelector('a-scene');
+    const arSystem = sceneEl.systems["mindar-image-system"];
+    const playButton = document.querySelector('#playbutton-object');
+    const videoObject = document.querySelector('#arvideo');
+
+    playButton.addEventListener('click', function () {
+      if (videoObject.paused == true) {
+        videoObject.play();
+        } else {
+        videoObject.pause();    }
+    }, false);
+
+    //Play Button
+    playButton.addEventListener("click", event =>{
+      if(playButton.object3D.visible == true)
+      {
+        playButton.setAttribute('visible',false);
+
+      }
+      else
+      {
+        playButton.setAttribute('visible',true);
+      }
+    },false);
+
+  });
