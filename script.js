5a995c67a393988f59024841cdc17cdbde7df050
Raj Sri Shanker <raj.ss166@gmail.com>
2021-12-13 23:12:26 -0500

    End of Session Rework 🏁

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const sceneEl = document.querySelector('a-scene');
-    const arSystem = sceneEl.systems["mindar-image-system"];
+    const targetMarker = document.querySelector('#marker-target')
     const playButton = document.querySelector('#playbutton-object');
     const videoObject = document.querySelector('#arvideo');
+    const linkedinObject = document.querySelector('#linkedin-object')
+    const twitterObject = document.querySelector('#twitter-object')
+    const githubObject = document.querySelector('#github-object')
+    const instagramObject = document.querySelector('#instagram-object')
+
 
     //Video Function
     playButton.addEventListener('click', function () {
@@ -15,6 +19,39 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }, false);
 
+     // detect target found
+     targetMarker.addEventListener("targetFound", event => {
+      linkedinObject.setAttribute('animation','property: position; to: 1.5 0.1 0');
+      twitterObject.setAttribute('animation','property: position; to: 1.5 -0.5 0');
+      githubObject.setAttribute('animation','property: position; to: 1.5 -1.1 0');
+      instagramObject.setAttribute('animation','property: position; to: 1.5 -1.7 0');
+    });
 
-    
+      // detect target lost
+      targetMarker.addEventListener("targetLost", event => {
+        linkedinObject.setAttribute('animation','property: position; to: 0 0 0');
+        twitterObject.setAttribute('animation','property: position; to: 0 0 0');
+        githubObject.setAttribute('animation','property: position; to: 0 0 0');
+        instagramObject.setAttribute('animation','property: position; to: 0 0 0');
+        playButton.setAttribute('visible',true);
+        videoObject.pause();
+      });
+
+      linkedinObject.addEventListener('click', function(){
+        window.open('https://www.linkedin.com/in/raj-sri-shanker-867b379b/');
+      });
+
+      twitterObject.addEventListener('click', function(){
+        window.open('https://twitter.com/RajSriShanker');
+      });
+
+      githubObject.addEventListener('click', function(){
+        window.open('https://github.com/RajSriShanker');
+      });
+
+      instagramObject.addEventListener('click', function(){
+        window.open('https://www.instagram.com/rajonthestreet/?hl=en');
+      });
+
   });
+
